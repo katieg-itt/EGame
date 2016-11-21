a2dceb74e1e20589531590eae135668e4219cf15
t00021385 <t00021385@students.ittralee.ie>
2016-11-21 23:20:36 +0000

    Added Enum to Category
    added game categories Enum,

--- src/GameCategory.java
+++ src/GameCategory.java
@@ -3,6 +3,29 @@
  */
 public enum GameCategory {
 
+    FPS, SPORTS, SIMULATIONS, MMO, ADVENTURE, RTS, PUZZLE, ACTION, COMBAT, STEALTH;
+
+    public String toString() {
+        switch (this) {
+            case FPS:
+                return "First Person Shooter";
+            case SPORTS:
+                return "Sports";
+            case SIMULATIONS:
+                return "Simulations";
+            case MMO: return "MMO";
+            case ADVENTURE: return "Adventure";
+            case RTS: return "Real Time Strategy";
+            case PUZZLE: return "Puzzle";
+            case ACTION: return "Action";
+            case COMBAT: return "Combat";
+            case STEALTH: return "Stealth";
 
 
-}
+
+        } //end switch
+
+
+        return null;
+    }// end toString()
+} //end class
